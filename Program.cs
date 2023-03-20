cdb110fcb349fd4f81b620a50ef2bef29db4070d
AlexSoft <4276038@gmail.com>
2023-03-21 00:00:13 +0300

    созданы комментарии для методов

--- Program.cs
+++ Program.cs
@@ -1,27 +1,34 @@
 ﻿using static System.Console;
 Clear();
 
-int n = int.Parse(Prompt("Введите значение n: "));
+int n = int.Parse(Prompt("Введите количество элементов исходного массива n: "));
 if (n < 1)
 {
     WriteLine("Кол-во элементов должно быть больше нуля!");
     return;
 }
+int max_len = int.Parse(Prompt("Введите минимальную длину строки для нового массива: "));
+if (max_len < 1)
+{
+    WriteLine("Минимаоьное кол-во символов в элементах массива должно быть больше 1!");
+    return;
+}
 string[] array_first = GetItems(n);
-string[] array_second = GetItemsLess4(array_first);
+string[] array_second = GetItemsLessN(array_first, max_len);
+
 PrintStringArray(array_first);
 Write(" -> ");
-PrintStringArray(array_second);
+PrintStringArray(array_second, false);
 
 // Методы
-
+// Метод запрашивает у пользователя вввод строки. 
 string Prompt(string intro, bool oneline = true)
 {
     Console.Write($"{intro}" + ((oneline) ? "" : "\n").ToString());
     string res = Console.ReadLine() ?? "";
     return res;
 }
-
+// Метод запрашивает у пользователя ввод определенного кол-ва элементов массива
 string[] GetItems(int n)
 {
     string[] result = new string[n];
@@ -31,31 +38,31 @@ string[] GetItems(int n)
     }
     return result;
 }
-
-int GetCountLes4(string[] array)
+// Метод считает кол-во элементов с длиной меньше n кол-ва символов
+int GetCountLesN(string[] array, int n)
 {
     int count = 0;
     foreach (var item in array)
     {
-        if (item.Length < 4) count++;
+        if (item.Length <= n) count++;
     }
     return count;
 }
-
-string[] GetItemsLess4(string[] array)
+// Метод возвращает массив элементов с длиной меньше n кол-ва символов из массива array
+string[] GetItemsLessN(string[] array, int n)
 {
-    string[] result = new string[GetCountLes4(array)];
+    string[] result = new string[GetCountLesN(array, n)];
     int i = 0;
     foreach (var item in array)
     {
-        if (item.Length < 4)
+        if (item.Length <= n)
         {
             result[i++] = item;
         }
     }
     return result;
 }
-
+// Метод печатает массив в виде [,] и согласно параметру oneline делает перевод строки или нет
 void PrintStringArray(string[] array, bool oneline = true)
 {
     Write("[\"" + string.Join("\", \"", array) + "\"]" + ((oneline) ? "" : "\n"));
